da46a959dff6e53efbbdd35b8999db81d1f008ce
Alvin Lionel <awilbert@student.ciputra.ac.id>
2023-06-09 08:13:06 +0700

    back to lowercase

--- app/Http/Controllers/AccController.php
+++ app/Http/Controllers/AccController.php
@@ -10,27 +10,27 @@ class AccController extends Controller
     public function show()
     {
         $id = session('customer')->CUST_ID;
-        $customer = DB::select('SELECT * FROM CUSTOMER WHERE CUST_ID = ?', [$id]);
+        $customer = DB::select('SELECT * FROM customer WHERE CUST_ID = ?', [$id]);
 
-        $results = DB::select('SELECT P.PROD_NAME, CP.CART_QTY, P.PROD_PRICE, (CP.CART_QTY * P.PROD_PRICE) AS PRICE, CP.CART_ID, P.PROD_ID
-                        FROM CART_PRODUCT CP
-                        LEFT JOIN CART C ON C.CART_ID = CP.CART_ID
-                        LEFT JOIN PRODUCT P ON P.PROD_ID = CP.PROD_ID
-                        WHERE C.CUST_ID = ?', [$id]);
+        $results = DB::select('SELECT p.PROD_NAME, cp.CART_QTY, p.PROD_PRICE, (cp.CART_QTY * p.PROD_PRICE) AS Price, cp.CART_ID, p.PROD_ID
+                      FROM cart_product cp
+                      LEFT JOIN cart c ON c.cart_id = cp.cart_id
+                      LEFT JOIN product p ON p.prod_id = cp.prod_id
+                      WHERE c.cust_id = ?', [$id]);
 
-        $wishlists = DB::select('SELECT P.PROD_NAME AS `NAME`, P.PROD_PRICE AS `PRICE`, P.PROD_ID AS `ID`, W.WISHLIST_ID AS WISHLIST_ID
-        FROM WISHLIST_PRODUCT WP
-        LEFT JOIN PRODUCT P
-        ON WP.PROD_ID = P.PROD_ID
-        LEFT JOIN WISHLIST W
-        ON W.WISHLIST_ID = WP.WISHLIST_ID
-        WHERE W.CUST_ID = ?;', [$id]);
+        $wishlists = DB::select('SELECT p.PROD_NAME as `name`, p.PROD_PRICE as `price`, p.PROD_ID as `id`, w.WISHLIST_ID as `wishlist_id`
+                        FROM wishlist_product wp
+                        left join product p
+                        on wp.PROD_ID = p.PROD_ID
+                        left join wishlist w
+                        on w.WISHLIST_ID = wp.WISHLIST_ID
+                        where w.CUST_ID = ?;', [$id]);
 
-        $total = DB::select('SELECT SUM(CP.CART_QTY * P.PROD_PRICE) AS PRICE
-        FROM CART_PRODUCT CP
-        LEFT JOIN CART C ON C.CART_ID = CP.CART_ID
-        LEFT JOIN PRODUCT P ON P.PROD_ID = CP.PROD_ID
-        WHERE C.CUST_ID = ?', [$id]);
+        $total = DB::select('SELECT sum(cp.CART_QTY * p.PROD_PRICE) AS Price
+                        FROM cart_product cp
+                        LEFT JOIN cart c ON c.cart_id = cp.cart_id
+                        LEFT JOIN product p ON p.prod_id = cp.prod_id
+                        WHERE c.cust_id = ?', [$id]);
 
         return view('account-profile', compact('customer', 'results', 'wishlists', 'total'));
     }
@@ -45,14 +45,14 @@ class AccController extends Controller
         $phone = $request->input('phone');
 
         // Check for duplicate username
-        $duplicateUsername = DB::select('SELECT * FROM CUSTOMER WHERE CUST_USERNAME = ? AND CUST_ID != ?', [$username, $userId]);
+        $duplicateUsername = DB::select('SELECT * FROM customer WHERE CUST_USERNAME = ? AND CUST_ID != ?', [$username, $userId]);
         if ($duplicateUsername) {
             // Handle duplicate username error
             return redirect()->back()->withErrors(['username' => 'Username is already taken.']);
         }
 
         // Update user profile
-        $query = 'UPDATE CUSTOMER SET CUST_USERNAME = ?, CUST_NAME = ?, CUST_GENDER = ?, CUST_EMAIL = ?, CUST_ADDRESS = ?, CUST_PHONE = ? WHERE CUST_ID = ?';
+        $query = 'UPDATE customer SET CUST_USERNAME = ?, CUST_NAME = ?, CUST_GENDER = ?, CUST_EMAIL = ?, CUST_ADDRESS = ?, cust_phone = ? WHERE CUST_ID = ?';
         DB::statement($query, [$username, $name, $gender, $email, $address, $phone, $userId]);
 
         // Redirect to profile page with success message
@@ -62,66 +62,68 @@ class AccController extends Controller
     {
 
         $id = session('customer')->CUST_ID;
-        $customer = DB::select('SELECT * FROM CUSTOMER WHERE CUST_ID = ?', [$id]);
-        $query = "SELECT P.PROD_NAME AS `NAME`, P.PROD_ID AS `ID`, TP.TRANS_QTY AS `QUANTITY`, TP.TRANS_PRICE AS `PRICE`, T.PAYMENT_STATUS AS `PAY`, DATE_FORMAT(T.TRANS_DATE, '%d-%M-%Y') AS DATE
-        FROM TRANSACTION_PRODUCT TP
-        LEFT JOIN TRANSACTION T ON T.TRANS_ID = TP.TRANS_ID
-        LEFT JOIN PRODUCT P ON P.PROD_ID = TP.PROD_ID
-        WHERE T.CUST_ID = ?;";
+        $customer = DB::select('SELECT * FROM customer WHERE CUST_ID = ?', [$id]);
+        $query = "select p.PROD_NAME as name, p.PROD_ID as id, tp.TRANS_QTY as quantity, tp.TRANS_PRICE as price, t.payment_status as 'pay', date_format(t.TRANS_DATE, '%d-%M-%Y') as `date`
+                    from transaction_product tp
+                    left join transaction t
+                    on t.trans_id = tp.trans_id
+                    left join product p
+                    on p.prod_id = tp.PROD_ID
+                    where t.CUST_ID = ?;";
         $trans = DB::select($query, [$id]);
 
-        $results = DB::select('SELECT P.PROD_NAME, CP.CART_QTY, P.PROD_PRICE, (CP.CART_QTY * P.PROD_PRICE) AS PRICE, CP.CART_ID, P.PROD_ID
-        FROM CART_PRODUCT CP
-        LEFT JOIN CART C ON C.CART_ID = CP.CART_ID
-        LEFT JOIN PRODUCT P ON P.PROD_ID = CP.PROD_ID
-        WHERE C.CUST_ID = ?', [$id]);
+        $results = DB::select('SELECT p.PROD_NAME, cp.CART_QTY, p.PROD_PRICE, (cp.CART_QTY * p.PROD_PRICE) AS Price, cp.CART_ID, p.PROD_ID
+                      FROM cart_product cp
+                      LEFT JOIN cart c ON c.cart_id = cp.cart_id
+                      LEFT JOIN product p ON p.prod_id = cp.prod_id
+                      WHERE c.cust_id = ?', [$id]);
 
-        $wishlists = DB::select('SELECT P.PROD_NAME AS `NAME`, P.PROD_PRICE AS `PRICE`, P.PROD_ID AS `ID`, W.WISHLIST_ID AS WISHLIST_ID
-        FROM WISHLIST_PRODUCT WP
-        LEFT JOIN PRODUCT P
-        ON WP.PROD_ID = P.PROD_ID
-        LEFT JOIN WISHLIST W
-        ON W.WISHLIST_ID = WP.WISHLIST_ID
-        WHERE W.CUST_ID = ?;', [$id]);
+        $wishlists = DB::select('SELECT p.PROD_NAME as `name`, p.PROD_PRICE as `price`, p.PROD_ID as `id`, w.WISHLIST_ID as `wishlist_id`
+                        FROM wishlist_product wp
+                        left join product p
+                        on wp.PROD_ID = p.PROD_ID
+                        left join wishlist w
+                        on w.WISHLIST_ID = wp.WISHLIST_ID
+                        where w.CUST_ID = ?;', [$id]);
 
-        $total = DB::select('SELECT SUM(CP.CART_QTY * P.PROD_PRICE) AS PRICE
-        FROM CART_PRODUCT CP
-        LEFT JOIN CART C ON C.CART_ID = CP.CART_ID
-        LEFT JOIN PRODUCT P ON P.PROD_ID = CP.PROD_ID
-        WHERE C.CUST_ID = ?;', [$id]);
+        $total = DB::select('SELECT sum(cp.CART_QTY * p.PROD_PRICE) AS Price
+                        FROM cart_product cp
+                        LEFT JOIN cart c ON c.cart_id = cp.cart_id
+                        LEFT JOIN product p ON p.prod_id = cp.prod_id
+                        WHERE c.cust_id = ?', [$id]);
 
         return view('account-transaction', compact('trans', 'results', 'wishlists', 'total', 'customer'));
     }
     public function showPassword()
     {
         $id = session('customer')->CUST_ID;
-        $customer = DB::select('SELECT * FROM CUSTOMER WHERE CUST_ID = ?', [$id]);
-        $results = DB::select('SELECT P.PROD_NAME, CP.CART_QTY, P.PROD_PRICE, (CP.CART_QTY * P.PROD_PRICE) AS PRICE, CP.CART_ID, P.PROD_ID
-                      FROM CART_PRODUCT CP
-                      LEFT JOIN CART C ON C.CART_ID = CP.CART_ID
-                      LEFT JOIN PRODUCT P ON P.PROD_ID = CP.PROD_ID
-                      WHERE C.CUST_ID = ?', [$id]);
+        $customer = DB::select('SELECT * FROM customer WHERE CUST_ID = ?', [$id]);
+        $results = DB::select('SELECT p.PROD_NAME, cp.CART_QTY, p.PROD_PRICE, (cp.CART_QTY * p.PROD_PRICE) AS Price, cp.CART_ID, p.PROD_ID
+                      FROM cart_product cp
+                      LEFT JOIN cart c ON c.cart_id = cp.cart_id
+                      LEFT JOIN product p ON p.prod_id = cp.prod_id
+                      WHERE c.cust_id = ?', [$id]);
 
-        $wishlists = DB::select('SELECT P.PROD_NAME AS `NAME`, P.PROD_PRICE AS `PRICE`, P.PROD_ID AS `ID`, W.WISHLIST_ID AS WISHLIST_ID
-                        FROM WISHLIST_PRODUCT WP
-                        LEFT JOIN PRODUCT P
-                        ON WP.PROD_ID = P.PROD_ID
-                        LEFT JOIN WISHLIST W
-                        ON W.WISHLIST_ID = WP.WISHLIST_ID
-                        WHERE W.CUST_ID = ?;', [$id]);
+        $wishlists = DB::select('SELECT p.PROD_NAME as `name`, p.PROD_PRICE as `price`, p.PROD_ID as `id`, w.WISHLIST_ID as `wishlist_id`
+                        FROM wishlist_product wp
+                        left join product p
+                        on wp.PROD_ID = p.PROD_ID
+                        left join wishlist w
+                        on w.WISHLIST_ID = wp.WISHLIST_ID
+                        where w.CUST_ID = ?;', [$id]);
 
-        $total = DB::select('SELECT SUM(CP.CART_QTY * P.PROD_PRICE) AS PRICE
-                        FROM CART_PRODUCT CP
-                        LEFT JOIN CART C ON C.CART_ID = CP.CART_ID
-                        LEFT JOIN PRODUCT P ON P.PROD_ID = CP.PROD_ID
-                        WHERE C.CUST_ID = ?', [$id]);
+        $total = DB::select('SELECT sum(cp.CART_QTY * p.PROD_PRICE) AS Price
+                        FROM cart_product cp
+                        LEFT JOIN cart c ON c.cart_id = cp.cart_id
+                        LEFT JOIN product p ON p.prod_id = cp.prod_id
+                        WHERE c.cust_id = ?', [$id]);
         return view('acc-password', compact('results', 'wishlists', 'total', 'customer'));
     }
     public function changePassword(Request $request)
     {
 
         $id = session('customer')->CUST_ID;
-        $query = 'SELECT CUST_PASSWORD FROM CUSTOMER WHERE CUST_ID = ?';
+        $query = 'SELECT CUST_PASSWORD FROM customer WHERE CUST_ID = ?';
         $customer = DB::select($query, [$id])[0]->CUST_PASSWORD;
         $oldPassword = $request->input('old_password');
         $newPassword = $request->input('new_password');
@@ -129,7 +131,7 @@ class AccController extends Controller
 
         if ($oldPassword === $customer) {
             if ($newPassword === $confirmPassword) {
-                DB::update('UPDATE CUSTOMER SET CUST_PASSWORD = ? WHERE CUST_ID = ?', [$newPassword, $id]);
+                DB::update('UPDATE customer SET CUST_PASSWORD = ? WHERE CUST_ID = ?', [$newPassword, $id]);
 
                 return redirect()->back()->with('success', 'Password changed successfully.');
             } else {
